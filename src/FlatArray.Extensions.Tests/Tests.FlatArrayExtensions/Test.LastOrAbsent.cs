1f5fd8edb36ed26bad6a022eadbe6efeccb26a4d
pmosk <pm@ggroupp.ru>
2022-11-24 11:39:41 +0400

    Use FlatArray 0.9

---
+++ src/FlatArray.Extensions.Tests/Tests.FlatArrayExtensions/Test.LastOrAbsent.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace GGroupp.Core.Collections.FlatArray.Extensions.Tests;
+
+partial class FlatArrayExtensionsTest
+{
+    [Fact]
+    public void LastOrAbsent_SourceIsEmpty_ExpectAbsent()
+    {
+        var source = FlatArray<RecordType?>.Empty;
+
+        var actual = source.LastOrAbsent();
+        var expected = Optional.Absent<RecordType?>();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData(null, MinusFifteen, One)]
+    [InlineData(MinusFifteen, Zero)]
+    public void LastOrAbsent_SourceIsNotEmpty_ExpectLastItem(int? last, params int?[] others)
+    {
+        var sourceBuilder = FlatArray<int?>.Builder.Create(others.Length + 1);
+
+        for (var i = 0; i < others.Length; i++)
+        {
+            sourceBuilder[i] = others[i];
+        }
+
+        sourceBuilder[others.Length] = last;
+
+        var source = sourceBuilder.Build();
+
+        var actual = source.LastOrAbsent();
+        var expected = Optional.Present(last);
+
+        Assert.Equal(expected, actual);
+    }
+}
